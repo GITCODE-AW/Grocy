c36ab72b1b92c42bf320f0cd729ec461f9d53387
GITCODE-AW <aditya.a.wadikar@gmail.com>
2023-12-30 18:59:48 +0530

    vercel deployment config

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
-pip install -r requirments.txt
-python 3.9 manage.py collectstatic
+#!/bin/bash
+
+# This script is for building static files
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
